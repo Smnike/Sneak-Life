f96b76edb2a3581262cd151d7dc57aabc39c54de
Savannah Cody <savannah.m.cody@gmail.com>
2018-01-10 10:43:07 -0500

    got shoe list page to render, though it doesnt list any shoes

--- controllers/sneakers.js
+++ controllers/sneakers.js
@@ -6,47 +6,48 @@ Router.get("/", (req, res) => {
     Shoes
     .find({})
     .then(sneakers => {
-        res.render("sneakers-index", { shoes })
+        res.render("sneakers-index", { sneakers })
     })
 })
-// Router.get("/sneakers", (req, res) => {
+
+Router.get("/sneakers", (req, res) => {
+    Shoes
+    .find({})
+    .then(sneakers => {
+        res.render("sneakers-index", { sneakers })
+    })
+})
+
+// Router.get("/sneakers/:brand/:name", (req, res) => {
 //     Shoes
-//     .find({})
-//     .then(sneakers => {
-//         res.render("sneakers-index", { shoes })
-//     }
+//     .findOne({brand: req.params.brand} && {name: req.params.name} && {price: req.params.price})
+//     .then(shoes => {
+//         res.render("sneakers-show", { sneakers })
+//     })
 // })
 
-Router.get("/sneakers/:brand", (req, res) => {
-    Shoes
-    .findOne({brand: req.params.brand} && {name: req.params.name} && {price: req.params.price})
-    .then(shoes => {
-        res.render("sneakers-show", { shoes })
-    })
-})
+// Router.post("/sneakers", (req, res) => {
+//     Shoes
+//     .create(req.body.shoes)
+//     .then(shoes => {
+//         res.redirect(`/sneakers/${shoes.brand} && ${shoes.name} && ${shoes.price}`)
+//     })
+// })
 
-Router.post("/sneakers", (req, res) => {
-    Shoes
-    .create(req.body.shoes)
-    .then(shoes => {
-        res.redirect(`/sneakers/${shoes.brand} && ${shoes.name} && ${shoes.price}`)
-    })
-})
+// Router.delete("/sneakers/:name", (req, res) => {
+//     Shoes
+//     .findOneAndRemove({brand: req.params.brand} && {name: req.params.name} && {price: req.params.price})
+//     .then(() => {
+//         res.redirect("/sneakers")
+//     })
+// })
 
-Router.delete("/sneakers/:name", (req, res) => {
-    Shoes
-    .findOneAndRemove({brand: req.params.brand} && {name: req.params.name} && {price: req.params.price})
-    .then(() => {
-        res.redirect("/sneakers")
-    })
-})
-
-Router.put("/sneakers/:name", (req, res) => {
-    Shoes
-    .findOneAndUpdate({brand: req.params.brand} && {name: req.params.name} && {price: req.params.price}, req.body.shoes, {new: true})
-    .then(shoes => {
-        res.redirect(`/sneakers/${shoes.brand} && ${shoes.name} && ${shoes.price}`)
-    })
-})
+// Router.put("/sneakers/:name", (req, res) => {
+//     Shoes
+//     .findOneAndUpdate({brand: req.params.brand} && {name: req.params.name} && {price: req.params.price}, req.body.shoes, {new: true})
+//     .then(shoes => {
+//         res.redirect(`/sneakers/${shoes.brand} && ${shoes.name} && ${shoes.price}`)
+//     })
+// })
 
 module.exports = Router
