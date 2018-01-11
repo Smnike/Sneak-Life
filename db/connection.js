e0bac4a21d7b89ed05c194ce6bc165121f57237f
Savannah Cody <savannah.m.cody@gmail.com>
2018-01-11 12:03:34 -0500

    edit connection.js

--- db/connection.js
+++ db/connection.js
@@ -5,16 +5,22 @@ mongoose.Promise = Promise
 
 const mongoUri = "mongodb://localhost/sneaklife"
 
-mongoose
-    .connect(mongoUri, { useMongoClient: true })
-    .then(connection =>
-        console.log(
-            `Connection established to db
-"${connection.db.databaseName}"`
-        )
-    )
-    .catch(connectionError =>
-        console.log("Connection failed!", connectionError)
-    )
+if (process.env.NODE_ENV == "production") {
+    mongoose.connect(process.env.MLAB_URL)
+  } else {
+    mongoose.connect(mongoUri);
+  }
+
+// mongoose
+//     .connect(mongoUri, { useMongoClient: true })
+//     .then(connection =>
+//         console.log(
+//             `Connection established to db
+// "${connection.db.databaseName}"`
+//         )
+//     )
+//     .catch(connectionError =>
+//         console.log("Connection failed!", connectionError)
+//     )
 
 module.exports = mongoose
